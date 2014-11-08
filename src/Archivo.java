2f630d5621682097297032b2387504be1c546b83
Max <maxdlr.7@gmail.com>
2014-11-08 13:42:30 -0600

    Arreglo en comentarios

--- src/Archivo.java
+++ src/Archivo.java
@@ -6,14 +6,19 @@
  * Aar�n Gir�n - 13042
  * Ricardo Franco - 13261
  */
+ // Se importa la libreria para lectura de ficheros en java
 import java.io.*;
-
+/*
+* Metodos y teoria para el uso de esta clase fue obtenida de la página 
+* http://www.discoduroderoer.es/clases-filereader-y-filewriter-para-ficheros-de-texto-en-java/
+* con la cual se trabajo para el uso y manejo de ficheros para dicha hoja de trabajo 
+*/
 public class Archivo{
     File archivo;
     FileReader fr;
     BufferedReader br;
     InterfazGrafo grafo = new GraphMatrix();    
-    
+// Se obtiene el nombre del archivo el cual ya esta creado y almacenado en la carpeta del proyecto
     public void obtenerArchivo() throws FileNotFoundException{
        archivo=new File("datos.txt"); 
     }  
